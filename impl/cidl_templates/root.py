eb66ac3e405434b6c32888250a7f23789bbc3e94
Gerwin Klein <gerwin.klein@proofcraft.systems>
2021-05-12 11:42:02 +1000

    Foundation setup and SPDX license tags
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- impl/cidl_templates/root.py
+++ impl/cidl_templates/root.py
@@ -1,14 +1,6 @@
+# Copyright 2016, Data61, CSIRO (ABN 41 687 119 230)
 #
-# Copyright 2016, Data61
-# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
-# ABN 41 687 119 230.
-#
-# This software may be distributed and modified according to the terms of
-# the BSD 2-Clause license. Note that NO WARRANTY is provided.
-# See "LICENSE_BSD2.txt" for details.
-#
-# @TAG(D61_BSD)
-#
+# SPDX-License-Identifier: BSD-2-Clause
 
 {{if header_mode}}
     #ifndef _RPC_INTERFACE_{{ifname.upper()}}_{{'CLIENT' if client_mode else 'SERVER'}}_H_\n
